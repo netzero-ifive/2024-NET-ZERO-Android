d82eb6fc96f0e806788aafb6a30c04860a0c4b77
joh <joh99111@gmail.com>
2024-07-20 04:53:39 +0900

    feat: Add Font

--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -1,20 +1,55 @@
 package com.example.myapplication.ui.theme
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+
+
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
+@RequiresApi(Build.VERSION_CODES.Q)
+val CustomFontFamily = FontFamily(
+    Font(R.font.pretendardstd_bold, FontWeight.Bold),
+    Font(R.font.pretendardstd_black, FontWeight.Black),
+    Font(R.font.pretendardstd_medium, FontWeight.Medium),
+    Font(R.font.pretendardstd_regular, FontWeight.Normal)
+)
 // Set of Material typography styles to start with
+@RequiresApi(Build.VERSION_CODES.Q)
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = CustomFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    bodySmall = TextStyle(
+        fontFamily = CustomFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = CustomFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = CustomFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
