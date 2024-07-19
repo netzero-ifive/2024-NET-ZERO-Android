af4017b94f338d171f9c088d54c2fb06033ab304
joh <joh99111@gmail.com>
2024-07-19 20:46:13 +0900

    feat: page for scanning

---
+++ app/src/main/java/com/example/myapplication/ui/screens/scan/ScanPage.kt
@@ -0,0 +1,82 @@
+package com.example.myapplication.ui.screens.scan
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+
+
+@Composable
+fun ScanPage() {
+    var scanResult by remember { mutableStateOf<String?>(null) }
+
+
+    val options = GmsBarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_AZTEC
+        )
+        .enableAutoZoom() // available on 16.1.0 and higher
+        .build()
+    val context = LocalContext.current
+    val scanner = GmsBarcodeScanning.getClient(context, options)
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Button(onClick = {
+            val intent = Intent(context, AllergyWarningActivity::class.java)
+            context.startActivity(intent)
+//            scanner.startScan()
+//                .addOnSuccessListener { barcode ->
+//
+//                }
+//                .addOnCanceledListener {
+//                    scanResult = "Scan cancelled"
+//                    Log.d("REZero", "취소")
+//                }
+//                .addOnFailureListener { e ->
+//                    scanResult = "Scan failed: ${e.message}"
+//                    Log.d("REZero", "실패 $e")
+//                }
+        }) {
+            Text("Start Scan")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        scanResult?.let {
+            Text(it)
+        }
+    }
+}
