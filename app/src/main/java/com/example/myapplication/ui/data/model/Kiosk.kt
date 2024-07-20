604331351b195144e38374c7d020a46121e2c213
joh <joh99111@gmail.com>
2024-07-20 09:13:46 +0900

    feat: kiosk data model

--- app/src/main/java/com/example/myapplication/ui/data/model/Kiosk.kt
+++ app/src/main/java/com/example/myapplication/ui/data/model/Kiosk.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication.ui.data.model
 
+
 data class Kiosk(
     val id: Int,
     val name: String,
     val address: String,
+    val image: String,
     val latitude: Double,
     val longitude: Double,
     val created_at: String,
